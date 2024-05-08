3e4fe895d955cca70fdf96a0c37a249565662fc9
Soufiane DAHIMI <soufianedahimi01@gmail.com>
2024-05-08 11:21:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
 from fastapi import FastAPI
+from tester import *
+
+
 
 app = FastAPI()
 
+
+
 @app.get("/")
-async def root():
-    return {"greeting": "Hello, World!", "message": "Welcome to FastAPI!"}
+def index():
+    """
+    This is the index page of the API
+    """
+    return {"message": "Hello World"}
+
+@app.post("/test/")
+def execute_test(test: Test):
+    """
+    This is the function to execute the test
+    """
+    return test.run_test()
+
+
+
+if __name__ == '__main__':
+    import uvicorn
+    import os
+    uvicorn.run("main:app", host="0.0.0.0", port=int(os.getenv("PORT", default=5000)), log_level="info")
+
